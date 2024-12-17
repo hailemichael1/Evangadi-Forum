3d9a6337d97451f83166570b67817e34dee1d4cc
hamidthedev1 <hamidabuhayat1@gmail.com>
2024-12-17 04:47:26 +0300

    header and footer components

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,11 +1,106 @@
 import React from 'react'
-
+import "./Footer.css"
+import "../Footer/Footer.css";
+import Container from "react-bootstrap/Container";
+import logo from "../../assets/evangadi-logo-footer.png";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 function Footer() {
-  return (
-    <div>
-      
-    </div>
-  )
+ 	return (
+    <>
+      <footer className='footer-container'>
+        <Container>
+          <Row>
+            <Col sm={12} md={4} className="my-3">
+              <Row>
+                <Col sm={12}>
+                  <img src={logo} alt="" />
+                </Col>
+                <Row
+                  style={{
+                    width: "70%",
+                    margin: "30px 0",
+                    fontSize: "25px",
+                  }}
+                >
+                  <Col sm={4}>
+                    <a
+                      href="https://www.facebook.com/EthiopiansNetwork"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-facebook text-white"></i>
+                    </a>
+                  </Col>
+
+                  <Col sm={4}>
+                    <a
+                      href="https://www.instagram.com/evangaditech/"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-instagram text-white"></i>
+                    </a>
+                  </Col>
+                  <Col sm={4}>
+                    <a
+                      href="https://www.youtube.com/c/weareethiopians"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-youtube text-white"></i>
+                    </a>
+                  </Col>
+                </Row>
+              </Row>
+            </Col>
+            <Col sm={12} md={4} className="my-3">
+              <h5 className="text-white">Useful Link</h5>
+              <ul
+                style={{
+                  listStyle: "none",
+                  marginLeft: "-25px",
+                  lineHeight: "30px",
+                }}
+              >
+                <li>
+                  <a href="https://www.evangadi.com/explained/">How it works</a>
+                </li>
+                <li>
+                  <a href="https://www.evangadi.com/legal/terms/">
+                    Terms of Service
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.evangadi.com/legal/privacy/">
+                    Privacy policy
+                  </a>
+                </li>
+              </ul>
+            </Col>
+            <Col sm={12} md={4} className="my-3">
+              <h5 className="text-white">Contact Info </h5>
+
+              <ul
+                style={{
+                  listStyle: "none",
+                  marginLeft: "-25px",
+                  lineHeight: "30px",
+                }}
+              >
+                <li>
+                  <a href="">Evangadi Networks</a>
+                </li>
+                <li>
+                  <a href="">contact@test.com</a>
+                </li>
+                <li>
+                  <a href="">+251-97-071-4410</a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
 }
 
 export default Footer
--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-
+import React from "react";
+import "./Header.css"
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import logo from "../../assets/evangadi-logo-black.png";
 function Header() {
-  return (
-    <div>
-      
-    </div>
-  )
+   return (
+     <>
+       <Navbar
+         bg="light"
+         expand="lg"
+         className="py-3 shadow-lg position-sticky w-100 "
+         style={{
+           zIndex: "99",
+           top: "0",
+         }}
+       >
+         <Container>
+           <Navbar.Brand href="/">
+             <img src={logo} alt="" />
+           </Navbar.Brand>
+           <Navbar.Toggle
+             aria-controls={`offcanvasNavbar-expand-lg`}
+           ></Navbar.Toggle>
+           <Navbar.Offcanvas
+             id={`offcanvasNavbar-expand-lg`}
+             aria-labelledby={`offcanvasNavbarLabel-expand-lg`}
+             placement="end"
+           >
+             <Offcanvas.Body>
+               <Nav className="justify-content-end flex-grow-1 ">
+                 <Nav.Link href="#">Home</Nav.Link>
+                 <Nav.Link href="#">How it works</Nav.Link>
+               </Nav>
+               
+                 <Nav>
+                   <Button className="px-5" variant="primary">
+                     SIGN IN
+                   </Button>
+                 </Nav>
+               
+             </Offcanvas.Body>
+           </Navbar.Offcanvas>
+         </Container>
+       </Navbar>
+     </>
+   );
 }
 
-export default Header
+export default Header;
