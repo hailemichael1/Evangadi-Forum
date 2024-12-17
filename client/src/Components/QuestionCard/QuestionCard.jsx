4560ebe172061b9b4a46aec2c276a47f3109a469
Adem Kedir <ademkedir724@gmail.com>
2024-12-17 17:43:10 +0300

    remove unnecessary content

--- client/src/Components/QuestionCard/QuestionCard.jsx
+++ client/src/Components/QuestionCard/QuestionCard.jsx
@@ -1,15 +1,15 @@
 import { card } from './QuestionCard.module.css';
 import { BsArrowRight } from 'react-icons/bs';
 function QuestionCard(Questions) {
-  console.log(JSON.stringify(Questions, null, 2));
   return (
     <div className={card}>
       <div>
+        {/*  */}
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWpfPWgt9O-1jZaIlxyoG-gz3sGQWT7LYMMw&s"
           alt=""
         />
-
+        {/* ADD USER NAME DYNAMICALLY */}
         <p>username</p>
       </div>
       <p>{Questions.Questions.title}</p>
