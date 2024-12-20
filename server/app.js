66b70c17e585c334b8417cb678e78e5fe6214473
Adem Kedir <ademkedir724@gmail.com>
2024-12-20 06:57:16 +0300

    handle invalid route on all type of action request

--- server/app.js
+++ server/app.js
@@ -1,39 +1,49 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5550;
 
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 // db connection
-const dbConnection = require("./db/dbConfig");
+const dbConnection = require('./db/dbConfig');
 
 // authorization middleware
-const authMiddleware = require("./middleware/authMiddleware");
+const authMiddleware = require('./middleware/authMiddleware');
 
 // user route middleware file
-const UserRoutes = require("./routes/userRoute");
+const UserRoutes = require('./routes/userRoute');
 
 //do questions middleware
-const questionsRoutes = require("./routes/questionRoute");
+const questionsRoutes = require('./routes/questionRoute');
 // answers middleware
-const answersRoutes = require("./routes/answerRoute");
+const answersRoutes = require('./routes/answerRoute');
 //json middleware to extract json data
 app.use(express.json());
 
 // user route middleware
-app.use("/api/users", UserRoutes);
+app.use('/api/users', UserRoutes);
 //questions routes middleware
-app.use("/api/questions", authMiddleware, questionsRoutes);
+app.use('/api/questions', authMiddleware, questionsRoutes);
 
 //answer routes middleware
 
-app.use("/api/answers", authMiddleware, answersRoutes);
+app.use('/api/answers', authMiddleware, answersRoutes);
+
+app.use(
+  '*',
+  app.all('*', (req, res, next) => {
+    console.log(req.url);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: 'An unexpected error occurred.' });
+  })
+);
 
 async function start() {
   try {
     const result = await dbConnection.execute("select'test' ");
     app.listen(port);
-    console.log("database connection established");
+    console.log('database connection established');
     console.log(`listening on port ${port}`);
   } catch (error) {
     console.log(error.message);
